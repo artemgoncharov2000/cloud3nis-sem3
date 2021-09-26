123eea4824af66c8f5dcb411f5b0044896af367e
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-09-26 22:17:23 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Feed from './src/components/Feed';
+import CreatePost from './src/components/CreatePost/CreatePost';
+import Post from './src/components/Post/Post';
 
+
+const Stack = createNativeStackNavigator();
+//  
+        //  <Stack.Screen name="Post" component={}/>
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Feed" 
+          component={Feed}
+        />
+        <Stack.Screen name="CreatePost" component={CreatePost}/>
+        <Stack.Screen name="Post" component={Post}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
